21667eb863196d09d4cf822ebd684d6df4d2223d
typesteam <t-rousey@att.net>
2019-11-02 18:13:38 -0500

    Testing options

--- options.js
+++ options.js
@@ -1,3 +1,49 @@
+const var ids = ["wikipedia", "gamepedia", "fandom"];
+
+var userRankings = [new Wikipedia(), new Gamepedia()];
+
+console.log(userRankings[0].query());
+
+class Wiki {
+	constructor(name, subId, subType) {
+		this.name = name;
+		this.subId = subId;
+		this.subType = subType;
+	}
+
+	function query() {}
+}
+
+class Wikipedia extends Wiki {
+	constructor() {
+		super(ids[0], "", "");
+	}
+
+	function query() {
+		return "wikipedia :)";
+	}
+}
+
+class Gamepedia extends Wiki {
+	constructor() {
+		super(ids[1], "", "");
+	}
+
+	function query() {
+		return "gamepedia :()";
+	}
+}
+
+class Fandom extends Wiki {
+	constructor() {
+		super(ids[2], "", "")
+	}
+
+	function query() {
+
+	}
+}
+
 // saves options to chrome.storage
 function save_options() {
 
