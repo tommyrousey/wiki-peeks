a97e779c30b1e0268ee9a6591f7b02ba3e76812d
ethane98 <ethane98@yahoo.com>
2019-11-02 17:52:11 -0500

    Right click and reorganize background

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 // called onload in the popup code
 function getPageDetails(callback) {
+
 	// inject the content script into the current page
 	chrome.tabs.executeScript(null, { file: 'content.js' });
 	// perform the callback when a message is received from the content script
@@ -7,3 +8,13 @@ function getPageDetails(callback) {
 	 	callback(message);
 	 });
 };
+rightClick = function(word){
+    var query = word.selectionText;
+    alert(query);
+ };
+
+chrome.contextMenus.create({
+ title: "wiki-peeks",
+ contexts:["selection"],  // ContextType
+ onclick: rightClick // A callback function
+});
