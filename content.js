8c0167e705619f4c036842ec620e816f9173049b
Benjamin Porter <bporter816@gmail.com>
2019-11-02 21:32:13 -0500

    Add page modification

--- content.js
+++ content.js
@@ -1,9 +1,25 @@
-alert('registering');
+console.log('registering');
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        alert('receiving');
-        if (request.id == 'msg') {
-            alert(request.msg);
+        console.log('receiving');
+        if (request.msg == 'hello') {
+            console.log(request.msg);
+            var selected = window.getSelection();
+            var domNode = selected.anchorNode;
+            var offset = selected.anchorOffset;
+            alert(domNode.nodeValue + ' a ' + offset);
+
+            domNode.nodeValue = '<strong>hellloooo</strong>';
+            /*
+            var spanTag = document.createElement('span');
+            spanTag.className = 'tooltiptext';
+
+            var newNode = document.createElement('div');
+            newNode.innerHTML = domNode.nodeValue;
+
+            newNode.appendChild(spanTag);
+            domNode = newNode;
+            */
         }
     }
 );
