adeaa44b4258c57da013758f8de239bec08330f4
Garrett Wezniak <gaarj4@gmail.com>
2019-11-02 20:50:35 -0500

    Read from user list of wikis

--- scripts/sortable.js
+++ scripts/sortable.js
@@ -1,7 +1,15 @@
 $(document).ready(function () {
     $(function () {
         var sortList = $("#sortable")
+
+        // Create a sortable list for the user to rearrange
         sortList.sortable();
         sortList.disableSelection();
+
+        // Display the wikis from the user's saved profile
+        userRankings.forEach(function (page) {
+            var listElement = '<li>' + page.name + '</li>';
+            sortList.append(listElement);
+        });
     });
 });
